7e9c5b56af4ff27d892f7f611111aa3798132d13
simonuks221 <simonasriauka@gmail.com>
2023-01-17 14:16:26 +0200

    Fixed corr function...

--- Correlation_function.vhd
+++ Correlation_function.vhd
@@ -8,6 +8,7 @@ entity Correlation_function is
 		function_length : integer := 50
 	);
 	port(
+		EN: in std_logic := '0';
 		CLK: in std_logic;
 		input_function : in double_array(0 to function_length - 1);
 		input_adc_values: in std_logic_vector(400-1 downto 0);
@@ -24,31 +25,39 @@ begin
 
 output_value <= std_logic_vector(to_unsigned(output_int, output_value'length));
 
-process(CLK)
+process(CLK, EN)
 variable temp_c: integer := 0;
 variable input_adc_values_reduced : std_logic_vector(200-1 downto 0);
+variable input_function_reduced : double_array(0 to 25 - 1);
 begin
 	if rising_edge(CLK) then
-		temp_c := 0;
-		if(first_part = '1') then
-			input_adc_values_reduced := input_adc_values(200-1 downto 0);
+		if(EN = '0') then
+			first_part <= '1';
+			output_int <= 0;
 		else
-			input_adc_values_reduced := input_adc_values(400-1 downto 200);
+			temp_c := 0;
+			if(first_part = '1') then
+				input_function_reduced := input_function(0 to 25-1);
+				input_adc_values_reduced := input_adc_values(200-1 downto 0);
+			else
+				input_function_reduced := input_function(25 to 50-1);
+				input_adc_values_reduced := input_adc_values(400-1 downto 200);
+			end if;
+			
+			for i in 0 to 25-1 loop
+				temp_c := temp_c + to_integer(unsigned((input_adc_values_reduced(i*8+8-1 downto i*8)))) * to_integer(unsigned((input_function_reduced(i))));
+			end loop;
+			--if(first_part = '0') then
+				--output_value <= std_logic_vector(to_unsigned(temp_c, output_value'length));
+			--end if;
+			if(first_part = '0') then
+				output_int <= temp_c + temp_int;
+				temp_int <= 0;
+			else
+				temp_int <= temp_c;
+			end if;
+			first_part <= not first_part;
 		end if;
-		
-		for i in 0 to 25-1 loop
-			temp_c := temp_c + to_integer(unsigned((input_adc_values(i*8+8-1 downto i*8)))) * to_integer(unsigned((input_function(i))));
-		end loop;
-		--if(first_part = '0') then
-			--output_value <= std_logic_vector(to_unsigned(temp_c, output_value'length));
-		--end if;
-		if(first_part = '0') then
-			output_int <= temp_c + temp_int;
-			temp_int <= 0;
-		else
-			temp_int <= temp_c;
-		end if;
-		first_part <= not first_part;
 	end if;
 end process;
 
