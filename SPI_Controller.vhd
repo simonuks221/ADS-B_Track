184580e6ce281150c283a8f1c7c669d813a15de2
simonuks221 <simonasriauka@gmail.com>
2023-03-01 18:54:38 +0200

    PCB STUFF

--- SPI_Controller.vhd
+++ SPI_Controller.vhd
@@ -17,11 +17,7 @@ port(
 	SEND_DATA: in std_logic_vector(7 downto 0) := "11110000";
 	SEND_READWRITE : in std_logic := '0';
 	SEND_IRQ: in std_logic := '0'
-	
 );
-
-
-
 end entity;
 
 architecture arc of SPI_Controller is
@@ -44,29 +40,34 @@ signal send_data_buffer : std_logic_vector(7 downto 0) := (others => '0');
 
 signal send_counter : integer range 0 to 16 := 0;
 signal send_CLK : std_logic := '0';
+signal set_variable : std_logic := '0';
+
+signal SCLK : std_logic := '1';
 
 begin
 
 send_clock_divider : Clock_divider port map(CLK => CLK, Prescaler => std_logic_vector(to_unsigned(SEND_CLK_PRESCALER, 16)), CLK_OUT => send_CLK);
+
+SPI_SCLK <= '1' when curr_state = idle else SCLK;
 
 process(CLK)
 begin
 	if(rising_edge(CLK))then
 		case curr_state is
 			when idle =>
-				if(SEND_IRQ = '1') then
+				if(SEND_IRQ = '1' and send_CLK = '0') then
 					curr_state <= readwrite;
 				end if;
 			when readwrite =>
-				--if(send_counter = 1) then
+				if(send_counter = 1) then
 					curr_state <= address;
-				--end if;
+				end if;
 			when address =>
-				if(send_counter = 7) then
+				if(send_counter = 8) then
 					curr_state <= data;
 				end if;
 			when data =>
-				if(send_counter = 15) then
+				if(send_counter = 16) then
 					curr_state <= idle;
 				end if;
 		end case;
@@ -75,30 +76,27 @@ end process;
 
 process(send_CLK)
 begin
-	if(rising_edge(send_CLK))then
-		case curr_state is
-			when idle =>
-				SPI_CS <= '1';
-				SPI_MOSI <= '0';
-				SPI_SCLK <= '0';
-			when readwrite =>
-				SPI_CS <= '0';
-				SPI_MOSI <= send_readwrite_buffer;
-			when address =>
-				SPI_MOSI <= send_address_buffer(send_counter);
-				send_counter <= send_counter + 1;
-			when data =>
-				SPI_MOSI <= send_data_buffer(send_counter-8);
-				send_counter <= send_counter + 1;
-		end case;
+	if(rising_edge(send_CLK)) then
+		SCLK <= not SCLK;
 	end if;
 end process;
 
-process(CLK)
+process(SCLK)
 begin
-	if(rising_edge(CLK))then
-		--send_CLK_counter <= send_CLK_Counter + 1;
-		
+	if(falling_edge(SCLK))then
+		case curr_state is
+			when idle =>
+
+			when readwrite =>
+				SPI_MOSI <= send_readwrite;
+				send_counter <= 1;
+			when address =>
+				send_counter <= send_counter + 1;
+				SPI_MOSI <= SEND_ADDRESS(send_counter);
+			when data =>
+				send_counter <= send_counter + 1;
+				SPI_MOSI <= SEND_DATA(send_counter-8);
+		end case;
 	end if;
 end process;
 
