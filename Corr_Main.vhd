7f8ea97d3be8411f93b20adcf1757571acc059d7
simonuks221 <simonasriauka@gmail.com>
2024-02-03 16:28:19 +0200

    feat: succesfull one bit corr

--- Corr_Main.vhd
+++ Corr_Main.vhd
@@ -25,8 +25,7 @@ port(
 );
 end entity;
 
-architecture arc of Corr_Main is
-
+architecture arc of Corr_Main is          --TestVoltage signalai "101101" ir "000010"
 
 component Corr_Buffer is
 	generic(
@@ -60,52 +59,72 @@ signal DATA_OUT_7 : std_logic_vector(BUFFER_LENGTH - 1 downto 0) := (others => '
 signal DATA_OUT_8 : std_logic_vector(BUFFER_LENGTH - 1 downto 0) := (others => '0');
 
 signal corr_value : integer range -50000 to 50000 := 0;
+signal corr_value1 : integer range -50000 to 50000 := 0;
+signal corr_value0 : integer range -50000 to 50000 := 0;
 
 --Preambules arrayus
-type coef_array is array(49 downto 0) of integer range -1 to 1;
-constant preambule_coef : coef_array := (1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+type coef_vector is array(0 to 49) of integer range 0 to 1; --WARNING: 0 at left, 49 at right of vector
+type coef_array is array(0 to 4) of coef_vector;
+type corr_state is (preambule, waiting, bits);
+
+constant preambule_coef : coef_vector := (1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 	0,	0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1);
+constant coef_00 : coef_vector := (0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	0,	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+constant coef_01 : coef_vector := (0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	0,	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+constant coef_10 : coef_vector := (1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	0,	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+constant coef_11 : coef_vector := (1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	0,	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+signal table_coef : coef_array := (preambule_coef, coef_00, coef_01, coef_10, coef_11);
+signal table_coef_idx : integer range 0 to 3 := 0;
 
 signal mram_write_cnt : integer range 0 to 15 := 0;
 signal address_counter : integer range 0 to MAX_ADDRESS_COUNTS+5 := 0;
 
-type state is (corr_pream, corr_bit, corr_crc);
-signal curr_state : state := corr_pream;
+signal curr_corr_state : corr_state := preambule;
 
 signal cnt : integer := 0;
 signal buffer_latch : std_logic := '0';
 
+signal waiting_cnt : integer range 0 to 30 := 0;--TODO: could use only one integer for counting? investigate
+signal bits_cnt : integer range 0 to 20 := 0; --For bit timing
+signal bits_idx : integer range 0 to 6 := 0;  --For bit tracking
+signal bits_data : std_logic_vector(5 downto 0) := (others => '0'); --Correlated bit value
+
 begin
-buff : corr_buffer generic map(BUFFER_LENGTH, BUFFER_WIDTH) port map(buffer_latch, DATA_IN, DATA_OUT_0, DATA_OUT_1, DATA_OUT_2, DATA_OUT_3, DATA_OUT_4, DATA_OUT_5, DATA_OUT_6, DATA_OUT_7, DATA_OUT_8);
+buff : corr_buffer generic map(BUFFER_LENGTH, BUFFER_WIDTH) port map(buffer_latch, DATA_IN, DATA_OUT_0, DATA_OUT_1, DATA_OUT_2, 
+                                                            DATA_OUT_3, DATA_OUT_4, DATA_OUT_5, DATA_OUT_6, DATA_OUT_7, DATA_OUT_8);
 
 buffer_latch <= '1' when cnt = 1 else '0';
 MRAM_ADDRESS_OUT <= std_logic_vector(to_unsigned(address_counter, MRAM_ADDRESS_OUT'length));
-MRAM_DATA_OUT <= "0000" & std_logic_vector(to_signed(corr_value, 12));
+--MRAM_DATA_OUT <= "0000" & std_logic_vector(to_unsigned(corr_value, 12)); --For debuging correlation value
+MRAM_DATA_OUT <= "0000000000" & bits_data; --For debugging correlated bit value
 
 CORR_DONE <= '1' when address_counter = MAX_ADDRESS_COUNTS else '0';
 DATA_IN <='0'&ADC_BITS(7 downto 0);
---DATA_IN <='0'&ADC_BITS(9 downto 2);
 
---Procesas lygiagrečiam sudėjimui
---https://surf-vhdl.com/vhdl-for-loop-statement/ --Efficient Binary loop addition
+--Efficient Binary loop addition
+--https://surf-vhdl.com/vhdl-for-loop-statement
 process(DATA_OUT_8, DATA_OUT_7, DATA_OUT_6, DATA_OUT_5, DATA_OUT_4, DATA_OUT_3, DATA_OUT_2, DATA_OUT_1, DATA_OUT_0, EN_CORR)
-type size is array (0 to   BUFFER_LENGTH-1) of signed(12 downto 0);
-type size1 is array (0 to (BUFFER_LENGTH/2)-1) of signed(12 downto 0);
-type size2 is array (0 to (BUFFER_LENGTH/4)-1) of signed(12 downto 0);
-type size3 is array (0 to (BUFFER_LENGTH/8)-1) of signed(12 downto 0);
-type size4 is array (0 to (BUFFER_LENGTH/16)-1) of signed(12 downto 0);
+type size is array (0 to   BUFFER_LENGTH-1) of unsigned(12 downto 0); --TODO: why 12? Shouldnt be 9?
+type size1 is array (0 to (BUFFER_LENGTH/2)-1) of unsigned(12 downto 0);
+type size2 is array (0 to (BUFFER_LENGTH/4)-1) of unsigned(12 downto 0);
+type size3 is array (0 to (BUFFER_LENGTH/8)-1) of unsigned(12 downto 0);
+type size4 is array (0 to (BUFFER_LENGTH/16)-1) of unsigned(12 downto 0);
 variable vacc : size; --50
 variable vacc1 : size1; --25
-variable vacc2 : size2; --12 --paliktas vienas
+variable vacc2 : size2; --12 --Left one
 variable vacc3 : size3; --6
 variable vacc4 : size4; --3
-variable vacc51 : signed(12 downto 0) := (others => '0');
-variable vacc52 : signed(12 downto 0) := (others => '0');
-variable a : signed(8 downto 0);
+variable vacc51 : unsigned(12 downto 0) := (others => '0');
+variable vacc52 : unsigned(12 downto 0) := (others => '0');
+variable a : unsigned(8 downto 0);
 begin
 	for i in 0 to BUFFER_LENGTH-1 loop
 		a := DATA_OUT_8(i)&DATA_OUT_7(i)&DATA_OUT_6(i)&DATA_OUT_5(i)&DATA_OUT_4(i)&DATA_OUT_3(i)&DATA_OUT_2(i)&DATA_OUT_1(i)&DATA_OUT_0(i);
-		vacc(i) := to_signed(to_integer(a) * preambule_coef(i), 13) ;
+		vacc(i) := to_unsigned(to_integer(a) * table_coef(table_coef_idx)(i), 13);
 	end loop;
 	
 	for i in 0 to (BUFFER_LENGTH/2)-1 loop
@@ -113,8 +132,8 @@ begin
 	end loop; 
 	
 	for i in 0 to (BUFFER_LENGTH/4)-1 loop
-		vacc2(i) := vacc1(i*2)+vacc1(i*2+1); --24 nepaimam
-	end loop; --paliktas vienas 50tas
+		vacc2(i) := vacc1(i*2)+vacc1(i*2+1); --24 left out
+	end loop; --50 left out
 	
 	for i in 0 to (BUFFER_LENGTH/8)-1 loop
 		vacc3(i) := vacc2(i*2)+vacc2(i*2+1);
@@ -128,6 +147,33 @@ begin
 	corr_value <= to_integer(vacc51) + to_integer(vacc52);
 end process;
 
+process(DATA_OUT_8, DATA_OUT_7, DATA_OUT_6, DATA_OUT_5, DATA_OUT_4, DATA_OUT_3, DATA_OUT_2, DATA_OUT_1, DATA_OUT_0, EN_CORR)
+type size is array (0 to 9) of unsigned(12 downto 0);
+variable vacc1 : size;
+variable vacc0 : size;
+
+variable corr_1 : integer := 0;
+variable corr_0 : integer := 0;
+
+variable a : unsigned(8 downto 0);
+begin
+	corr_1 := 0;
+	corr_0 := 0;
+	for i in 0 to 9 loop
+		a := DATA_OUT_8(i)&DATA_OUT_7(i)&DATA_OUT_6(i)&DATA_OUT_5(i)&DATA_OUT_4(i)&DATA_OUT_3(i)&DATA_OUT_2(i)&DATA_OUT_1(i)&DATA_OUT_0(i);
+		vacc0(i) := to_unsigned(to_integer(a) * table_coef(1)(i), 13); --TODO: separate arrays
+		vacc1(i) := to_unsigned(to_integer(a) * table_coef(3)(i), 13);
+	end loop;
+	
+	for i in 0 to 9 loop --Do not need two loops, PARALERISE
+		corr_1 := corr_1 + to_integer(vacc1(i));
+		corr_0 := corr_0 + to_integer(vacc0(i));
+	end loop;
+	corr_value0 <= corr_1; --TODO: why need to be switched?
+	corr_value1 <= corr_0;
+
+end process;
+
 process(CLK)
 begin
 	if rising_edge(CLK) then
@@ -136,6 +182,12 @@ begin
 			MRAM_WRITE_DATA <= '0';
 			address_counter <= 0;
 			PREAMBULE_FOUND <= '0';
+			curr_corr_state <= preambule;
+			table_coef_idx <= 0;
+			waiting_cnt <= 0;
+			bits_data <= (others => '0');
+			bits_cnt <= 0;
+			bits_idx <= 0;
 		else
 			PREAMBULE_FOUND <= '0';
 			MRAM_WRITE_DATA <= '0';
@@ -145,19 +197,49 @@ begin
 				cnt <= cnt + 1;
 				case cnt is 
 					when 1 =>
-					
+						--Left empty for addition delay
 					when 2 =>
-					
+						--Left empty for addition delay
 					when 3=>
-						if corr_value > 460 or corr_value = 460 then
-							PREAMBULE_FOUND <= '1';
-						else
-							
-						end if;
+						case curr_corr_state is
+							when preambule =>
+								waiting_cnt <= 0;
+								bits_cnt <= 0;
+								bits_idx <= 0;
+								bits_data <= (others => '0');
+								if corr_value > 3655 or corr_value = 3655 then
+									PREAMBULE_FOUND <= '1';
+									curr_corr_state <= waiting;
+								end if;
+							when waiting =>
+								if waiting_cnt = 29 then
+									curr_corr_state <= bits;
+									waiting_cnt <= 0;
+								else
+									waiting_cnt <= waiting_cnt + 1;
+								end if;
+							when bits =>
+								if bits_cnt = 9 then
+									bits_cnt <= 0;
+									if corr_value1 > corr_value0 then
+										--Correlated 1
+										bits_data <= bits_data(4 downto 0) & '1';
+									else
+										--Correlated 0
+										bits_data <= bits_data(4 downto 0) & '0';	
+									end if;
+									if bits_idx = 6 then
+										curr_corr_state <= preambule;
+									else
+										bits_idx <= bits_idx + 1;
+									end if;
+								else
+									bits_cnt <= bits_cnt + 1;
+								end if;
+						end case;
 						cnt <= 0;
 						address_counter <= address_counter + 1;
 						MRAM_WRITE_DATA <= '1';
-						
 					when others =>
 						cnt <= 0;
 				end case;
