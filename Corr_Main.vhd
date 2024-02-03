0991298068aebef8b05fe11eb8797abf324134aa
simonuks221 <simonasriauka@gmail.com>
2024-02-03 17:13:22 +0200

    feat: two bit correlation

--- Corr_Main.vhd
+++ Corr_Main.vhd
@@ -25,7 +25,7 @@ port(
 );
 end entity;
 
-architecture arc of Corr_Main is          --TestVoltage signalai "101101" ir "000010"
+architecture arc of Corr_Main is          --TestVoltage signals "101101" ir "000010"
 
 component Corr_Buffer is
 	generic(
@@ -57,28 +57,35 @@ signal DATA_OUT_5 : std_logic_vector(BUFFER_LENGTH - 1 downto 0) := (others => '
 signal DATA_OUT_6 : std_logic_vector(BUFFER_LENGTH - 1 downto 0) := (others => '0');
 signal DATA_OUT_7 : std_logic_vector(BUFFER_LENGTH - 1 downto 0) := (others => '0');
 signal DATA_OUT_8 : std_logic_vector(BUFFER_LENGTH - 1 downto 0) := (others => '0');
-
+--Peambule correlation
 signal corr_value : integer range -50000 to 50000 := 0;
-signal corr_value1 : integer range -50000 to 50000 := 0;
-signal corr_value0 : integer range -50000 to 50000 := 0;
+--Correlations for first/newer bit
+signal corr_value_first_1 : integer range -50000 to 50000 := 0;
+signal corr_value_first_0 : integer range -50000 to 50000 := 0;
+--Correlations for second/older bit
+signal corr_value_second_1 : integer range -50000 to 50000 := 0;
+signal corr_value_second_0 : integer range -50000 to 50000 := 0;
 
 --Preambules arrayus
-type coef_vector is array(0 to 49) of integer range 0 to 1; --WARNING: 0 at left, 49 at right of vector
-type coef_array is array(0 to 4) of coef_vector;
+type preambule_coef_vector is array(0 to 49) of integer range 0 to 1; --WARNING: 0 at left, 49 at right of vector
+type bit_coef_vector is array(0 to 9) of integer range 0 to 1; --WARNING: 0 at left, 49 at right of vector
+--type coef_array is array(0 to 4) of coef_vector;
 type corr_state is (preambule, waiting, bits);
 
-constant preambule_coef : coef_vector := (1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-	0,	0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1);
-constant coef_00 : coef_vector := (0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-	0,	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
-constant coef_01 : coef_vector := (0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-	0,	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
-constant coef_10 : coef_vector := (1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-	0,	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
-constant coef_11 : coef_vector := (1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-	0,	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
-signal table_coef : coef_array := (preambule_coef, coef_00, coef_01, coef_10, coef_11);
-signal table_coef_idx : integer range 0 to 3 := 0;
+constant preambule_coef : preambule_coef_vector := (1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 
+																	0, 0, 0, 0, 0, 0, 0, 0, 0, 0,	0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1);
+constant bit_1_coef : bit_coef_vector := (1, 1, 1, 1, 1, 0, 0, 0, 0, 0);
+constant bit_0_coef : bit_coef_vector := (0, 0, 0, 0, 0, 1, 1, 1, 1, 1);
+--constant coef_00 : coef_vector := (0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+--	0,	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+--constant coef_01 : coef_vector := (0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+--	0,	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+--constant coef_10 : coef_vector := (1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+--	0,	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+--constant coef_11 : coef_vector := (1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+--	0,	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+--signal table_coef : coef_array := (preambule_coef, coef_00, coef_01, coef_10, coef_11);
+--signal table_coef_idx : integer range 0 to 3 := 0;
 
 signal mram_write_cnt : integer range 0 to 15 := 0;
 signal address_counter : integer range 0 to MAX_ADDRESS_COUNTS+5 := 0;
@@ -97,7 +104,7 @@ begin
 buff : corr_buffer generic map(BUFFER_LENGTH, BUFFER_WIDTH) port map(buffer_latch, DATA_IN, DATA_OUT_0, DATA_OUT_1, DATA_OUT_2, 
                                                             DATA_OUT_3, DATA_OUT_4, DATA_OUT_5, DATA_OUT_6, DATA_OUT_7, DATA_OUT_8);
 
-buffer_latch <= '1' when cnt = 1 else '0';
+--buffer_latch <= '1' when cnt = 1 else '0';
 MRAM_ADDRESS_OUT <= std_logic_vector(to_unsigned(address_counter, MRAM_ADDRESS_OUT'length));
 --MRAM_DATA_OUT <= "0000" & std_logic_vector(to_unsigned(corr_value, 12)); --For debuging correlation value
 MRAM_DATA_OUT <= "0000000000" & bits_data; --For debugging correlated bit value
@@ -124,7 +131,7 @@ variable a : unsigned(8 downto 0);
 begin
 	for i in 0 to BUFFER_LENGTH-1 loop
 		a := DATA_OUT_8(i)&DATA_OUT_7(i)&DATA_OUT_6(i)&DATA_OUT_5(i)&DATA_OUT_4(i)&DATA_OUT_3(i)&DATA_OUT_2(i)&DATA_OUT_1(i)&DATA_OUT_0(i);
-		vacc(i) := to_unsigned(to_integer(a) * table_coef(table_coef_idx)(i), 13);
+		vacc(i) := to_unsigned(to_integer(a) * preambule_coef(i), 13);--table_coef(table_coef_idx)(i), 13);
 	end loop;
 	
 	for i in 0 to (BUFFER_LENGTH/2)-1 loop
@@ -151,30 +158,55 @@ process(DATA_OUT_8, DATA_OUT_7, DATA_OUT_6, DATA_OUT_5, DATA_OUT_4, DATA_OUT_3, 
 type size is array (0 to 9) of unsigned(12 downto 0);
 variable vacc1 : size;
 variable vacc0 : size;
-
 variable corr_1 : integer := 0;
 variable corr_0 : integer := 0;
-
 variable a : unsigned(8 downto 0);
 begin
 	corr_1 := 0;
 	corr_0 := 0;
 	for i in 0 to 9 loop
 		a := DATA_OUT_8(i)&DATA_OUT_7(i)&DATA_OUT_6(i)&DATA_OUT_5(i)&DATA_OUT_4(i)&DATA_OUT_3(i)&DATA_OUT_2(i)&DATA_OUT_1(i)&DATA_OUT_0(i);
-		vacc0(i) := to_unsigned(to_integer(a) * table_coef(1)(i), 13); --TODO: separate arrays
-		vacc1(i) := to_unsigned(to_integer(a) * table_coef(3)(i), 13);
+		vacc0(i) := to_unsigned(to_integer(a) * bit_0_coef(i), 13); --TODO: separate arrays
+		vacc1(i) := to_unsigned(to_integer(a) * bit_1_coef(i), 13);
 	end loop;
 	
 	for i in 0 to 9 loop --Do not need two loops, PARALERISE
 		corr_1 := corr_1 + to_integer(vacc1(i));
 		corr_0 := corr_0 + to_integer(vacc0(i));
 	end loop;
-	corr_value0 <= corr_1; --TODO: why need to be switched?
-	corr_value1 <= corr_0;
+	corr_value_first_0 <= corr_1; --TODO: why need to be switched?
+	corr_value_first_1 <= corr_0;
+end process;
 
+process(DATA_OUT_8, DATA_OUT_7, DATA_OUT_6, DATA_OUT_5, DATA_OUT_4, DATA_OUT_3, DATA_OUT_2, DATA_OUT_1, DATA_OUT_0, EN_CORR)
+type size is array (0 to 9) of unsigned(12 downto 0);
+variable vacc1 : size;
+variable vacc0 : size;
+variable corr_1 : integer := 0;
+variable corr_0 : integer := 0;
+variable a : unsigned(8 downto 0);
+begin
+	corr_1 := 0;
+	corr_0 := 0;
+	for i in 10 to 19 loop
+		a := DATA_OUT_8(i)&DATA_OUT_7(i)&DATA_OUT_6(i)&DATA_OUT_5(i)&DATA_OUT_4(i)&DATA_OUT_3(i)&DATA_OUT_2(i)&DATA_OUT_1(i)&DATA_OUT_0(i);
+		vacc0(i-10) := to_unsigned(to_integer(a) * bit_0_coef(i-10), 13); --TODO: separate arrays
+		vacc1(i-10) := to_unsigned(to_integer(a) * bit_1_coef(i-10), 13);
+	end loop;
+	
+	for i in 10 to 19 loop --Do not need two loops, PARALERISE
+		corr_1 := corr_1 + to_integer(vacc1(i-10));
+		corr_0 := corr_0 + to_integer(vacc0(i-10));
+	end loop;
+	corr_value_second_0 <= corr_1; --TODO: why need to be switched?
+	corr_value_second_1 <= corr_0;
 end process;
 
 process(CLK)
+variable corr_00 : integer := 0;
+variable corr_01 : integer := 0;
+variable corr_10 : integer := 0;
+variable corr_11 : integer := 0;
 begin
 	if rising_edge(CLK) then
 		if(EN_CORR = '0') then
@@ -183,11 +215,12 @@ begin
 			address_counter <= 0;
 			PREAMBULE_FOUND <= '0';
 			curr_corr_state <= preambule;
-			table_coef_idx <= 0;
+			--table_coef_idx <= 0;
 			waiting_cnt <= 0;
 			bits_data <= (others => '0');
 			bits_cnt <= 0;
 			bits_idx <= 0;
+			buffer_latch <= '0';
 		else
 			PREAMBULE_FOUND <= '0';
 			MRAM_WRITE_DATA <= '0';
@@ -195,8 +228,10 @@ begin
 				cnt <= 1;
 			else
 				cnt <= cnt + 1;
+				buffer_latch <= '0';
 				case cnt is 
 					when 1 =>
+					buffer_latch <= '1';
 						--Left empty for addition delay
 					when 2 =>
 						--Left empty for addition delay
@@ -219,19 +254,32 @@ begin
 									waiting_cnt <= waiting_cnt + 1;
 								end if;
 							when bits =>
-								if bits_cnt = 9 then
+								if bits_cnt = 19 then
 									bits_cnt <= 0;
-									if corr_value1 > corr_value0 then
-										--Correlated 1
-										bits_data <= bits_data(4 downto 0) & '1';
+									corr_00 := corr_value_second_0 + corr_value_first_0;
+									corr_01 := corr_value_second_0 + corr_value_first_1;
+									corr_10 := corr_value_second_1 + corr_value_first_0;
+									corr_11 := corr_value_second_1 + corr_value_first_1;
+									if corr_00 > corr_11 and corr_00 > corr_01 and corr_00 > corr_10 then
+										bits_data <= bits_data(3 downto 0) & "00";
+									elsif corr_01 > corr_11 and corr_01 > corr_00 and corr_01 > corr_10 then
+										bits_data <= bits_data(3 downto 0) & "01";
+									elsif corr_10 > corr_11 and corr_10 > corr_01 and corr_10 > corr_00 then
+										bits_data <= bits_data(3 downto 0) & "10";
 									else
-										--Correlated 0
-										bits_data <= bits_data(4 downto 0) & '0';	
+										bits_data <= bits_data(3 downto 0) & "11";
 									end if;
+--									if corr_value_first_1 > corr_value_first_0 then
+--										--Correlated 1
+--										bits_data <= bits_data(4 downto 0) & '1';
+--									else
+--										--Correlated 0
+--										bits_data <= bits_data(4 downto 0) & '0';	
+--									end if;
 									if bits_idx = 6 then
 										curr_corr_state <= preambule;
 									else
-										bits_idx <= bits_idx + 1;
+										bits_idx <= bits_idx + 2; --Because correlating 2 bits at a time
 									end if;
 								else
 									bits_cnt <= bits_cnt + 1;
