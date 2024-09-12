61641b400b51c3be6e0c85f38396c148ee70039e
simonuks221 <simonasriauka@gmail.com>
2024-09-12 21:15:51 +0300

    feat: improved multiprocessing with guard rails to not break the PC

--- Matlab/ADSB_Signal_statistics_generator.py
+++ Matlab/ADSB_Signal_statistics_generator.py
@@ -8,18 +8,23 @@ from preambules_list import preambule_list
 step_amount = 10 # How many steps thru amplitude
 min_generation_amplitude = 0.2
 max_generation_amplitude = 1.1
-step_generation_amount = 5 # How many ADS-B signals to generate each step
+step_generation_amount = 30 # How many ADS-B signals to generate each step (same amplitude and different preambule masks)
 
-def monitor_completion_process(result_queue: multiprocessing.Queue, total_work: int):
+total_generations = step_generation_amount * step_amount
+all_amplitudes = np.linspace(min_generation_amplitude, max_generation_amplitude, step_amount)
+total_work = len(all_amplitudes) * step_generation_amount
+
+def monitor_completion_process(result_queue: multiprocessing.Queue, total_work: int, processes_limit_semaphore):
     work_done = 0
     start_time = time.time()
     print_loading_bar(0, total_work, start_time)
     while(True):
-        #Wait for all work to be done
+        # Wait for all work to be done
         result = result_queue.get()
         work_done += 1
         print_loading_bar(work_done, total_work, start_time)
-        result_queue.task_done()
+        # result_queue.task_done()
+        processes_limit_semaphore.release()
         if work_done == total_work:
             break
 
@@ -30,11 +35,10 @@ def print_loading_bar(iteration: int, total: int, start_time: time, length: int 
     elapsed_time = time.time() - start_time
     print(f'\rProgress: |{bar}| {percent}% Complete {"{:.2f}".format(elapsed_time)}s')
 
-def adsb_values_generator_process(amplitude: int, this_step: int, full_bits: bytes, result_queue: multiprocessing.Queue, processes_limit_semaphore):
+def adsb_values_generator_process(amplitude_i: int, this_step: int, full_bits: bytes):
     process_points = np.zeros(len(preambule_list))
     process_energies = np.zeros(len(preambule_list))
-    # for generation_i in range(step_generation_amount):
-    curr_signal = generate_ADSB(amplitude, full_bits)[2]
+    curr_signal = generate_ADSB(all_amplitudes[amplitude_i], full_bits)[2]
     digitized_signal = digitize_signal(curr_signal, 100E6, 10E6, 1.4, 2**10)[0]
     for preambule_i in range(len(preambule_list)):
         curr_preambule = preambule_list[preambule_i]
@@ -43,9 +47,7 @@ def adsb_values_generator_process(amplitude: int, this_step: int, full_bits: byt
         # Get all energy
         curr_energy = get_signal_energy(digitized_signal, len(curr_preambule.get_coefficients()))
         process_energies[preambule_i] = curr_energy[curr_preambule.get_expected_maximum()]
-    # Worker done, return the results
-    result_queue.put(1)
-    processes_limit_semaphore.release()
+    return (amplitude_i, this_step, process_points, process_energies)
 
 if __name__ == "__main__":
     print('Start')
@@ -55,31 +57,36 @@ if __name__ == "__main__":
     crc_bits = generate_adsb_crc(data_bits)
     full_bits = data_bits + crc_bits
 
-    total_generations = step_generation_amount * step_amount
-    all_amplitudes = np.linspace(min_generation_amplitude, max_generation_amplitude, step_amount)
-
-    all_energies = np.zeros((len(preambule_list), step_amount, step_generation_amount))
-    all_points = np.zeros((len(preambule_list), step_amount, step_generation_amount))
-
-    # Distribute workload to different processes
-    # Each worker is responsible for
-    result_queue = multiprocessing.JoinableQueue()
-    total_work = len(all_amplitudes) * step_generation_amount
-    processes_limit = multiprocessing.cpu_count() * 2
-    processes_limit_semaphore = multiprocessing.Semaphore(processes_limit)
-    workers = []
-    monitor_process = multiprocessing.Process(target=monitor_completion_process, args=(result_queue, total_work))
-    monitor_process.start()
+    pool = multiprocessing.Pool(processes = multiprocessing.cpu_count())
+    results = []
+    start_time = time.time()
     for amplitude_i in range(len(all_amplitudes)):
         for step_i in range(step_generation_amount):
-            processes_limit_semaphore.acquire()
-            worker_process = multiprocessing.Process(target=adsb_values_generator_process, args=(all_amplitudes[amplitude_i], step_i, full_bits, result_queue, processes_limit_semaphore))
-            worker_process.start()
-            workers.append(worker_process)
-    # Wait for workers to finish
-    for worker_process in workers:
-        worker_process.join()
-    monitor_process.join()
+            result = pool.apply_async(adsb_values_generator_process, (amplitude_i, step_i, full_bits))    # evaluate "f(10)" asynchronously
+            results.append(result)
+    pool.close()
+    # Processes are running
+    last_results_len = -1
+    while(True):
+        results_len = sum(1 for r in results if r.ready())
+        if(results_len != last_results_len):
+            last_results_len = results_len
+            print_loading_bar(last_results_len, total_work, start_time)
+            if(last_results_len == total_work):
+                break
+        time.sleep(1)
+
+    pool.join()
+    # Get all results and append them together
+    print("Calculating results")
+    all_energies = np.zeros((len(preambule_list), step_amount, step_generation_amount))
+    all_points = np.zeros((len(preambule_list), step_amount, step_generation_amount))
+    for result in results:
+        result_amplitude_i, result_step, result_points, result_energies = result.get()
+        all_energies[:, result_amplitude_i, result_step] = result_energies
+        all_points[:, result_amplitude_i, result_step] = result_points
+
+    print("Finished calculating results")
 
     # Calculate energy metrics
     mean_energies = np.zeros((len(preambule_list), step_amount))
