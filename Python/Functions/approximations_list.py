62a1f17acba6c9df57a47bb9957d6bd7e3c48dab
simonuks221 <simonasriauka@gmail.com>
2025-01-29 22:14:01 +0200

    feat: approximations refactor into class

--- Python/Functions/approximations_list.py
+++ Python/Functions/approximations_list.py
@@ -1,21 +1,58 @@
 from enum import Enum
+import numpy as np
+from preambules_list import Preambule
+from Functions.signal_generator import generate_approx
 
 
 class Approximation(Enum):
     MinQuantile = 0
     Mean = 1
     MaxQuantile = 2
-    Last = 3
+    LastCommon = 3
+    RestrictedLow = 4
+    Last = 5
 
 
 _APPROXIMATION_STRINGS = ["Min quantile", "Mean", "Max quantile"]
 
 
-def get_approximation_name(approximation) -> str:
-    if isinstance(approximation, Approximation):
-        index = approximation.value
-    elif isinstance(approximation, int):
-        index = approximation
-    else:
-        raise ValueError("Input must be of type Approximation or int")
-    return _APPROXIMATION_STRINGS[index]
+class ApproximationBase:
+    def __init__(
+        self,
+        preamble: Preambule,
+        approximation_type: Approximation,
+        input_x: np.ndarray,
+        input_y: np.ndarray,
+    ):
+        self.preamble = preamble
+        self.approximation_type = approximation_type
+        self.input_x = input_x
+        self.input_y = input_y
+        self.coefficients = []
+
+    def get_name(self) -> str:
+        return _APPROXIMATION_STRINGS[self.approximation_type.value]
+
+    def generate_approximation(self, energy: np.ndarray) -> np.ndarray:
+        return generate_approx(energy, self.coefficients)
+
+
+class ApproximationRestrictedLow(ApproximationBase):
+    def __init__(
+        self,
+        preamble: Preambule,
+        approximation_type: Approximation,
+        input_x: np.ndarray,
+        input_y: np.ndarray,
+        min_threshold: int,
+    ):
+        super().__init__(preamble, approximation_type, input_x, input_y)
+        self._min_threshold = min_threshold
+
+    def generate_approximation(self, energy: np.ndarray) -> np.ndarray:
+        approximation = super().generate_approximation(energy)
+        approximation[approximation < self._min_threshold] = self._min_threshold
+        return approximation
+
+    def get_name(self) -> str:
+        return "Restricted low"
